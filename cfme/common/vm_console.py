0c361da143a7e56b850ad63cf934d71a06b32516
James Olin Oden <joden@redhat.com>
2017-05-17 10:48:15 -0400

    Create HTML5 VM Console Test
    Test runs against VM/Instances on RHV, OpenStack and VMware.
    Tests that the HTML5 VM Console feature works.   Attempts to
    login to a console on a VM/Instance on the specified provider
    touch a file, and verify the files existence.
    
    Also added to cfme/common/vm.py:
    
        - open_console method
        - console method
    
    A new class VMConsole is added to abstract the console functionality
    of:
    
        - reading the console banner
        - sending text to the console
        - reading the console bitmap
    
    All of this while managing switching from the CFME tab to the console
    tab and back.
    
    (FIXES RHCFQE-2647)

---
+++ cfme/common/vm_console.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""Module containing classes with common behaviour for consoles of both VMs and Instances of all types."""
+
+import base64
+import re
+
+from utils.log import logger
+from utils.pretty import Pretty
+from wait_for import wait_for
+
+banner_id = 'noVNC_status'
+canvas_id = 'noVNC_canvas'
+ctrl_alt_del_id = 'sendCtrlAltDelButton'
+
+class VMConsole(Pretty):
+    """
+    Class to manage the VM Console.   Presently, only support HTML5 Console.
+    """
+    pretty_attrs = ['appliance_handle', 'browser', 'console_handle', 'name']
+
+    def __init__(self, name, selenium, console_handle, appliance_handle):
+        self.name = name
+        self.selenium = selenium
+        self.console_handle = console_handle
+        self.appliance_handle = appliance_handle
+
+    ###
+    # Methods
+    #
+    def get_banner(self):
+        '''
+        Gets the text of the banner above the console screen.
+        '''
+        self.switch_to_console()
+        text = self.selenium.find_element_by_id(banner_id).text
+        logger.info('Read following text from console banner: {}'.format(text))
+        self.switch_to_appliance()
+        return text
+
+    def get_screen(self):
+        '''
+        Retrieves the bit map from the canvas widget that represents the
+        console screen.   Returns it as a binary string.
+
+        Implementation:
+        The canvas tag has a method toDataURL() which one can use in javascript to
+        obtain the canvas image  base64 encoded.   Examples of how to do this can be
+        seen here:
+
+            https://qxf2.com/blog/selenium-html5-canvas-verify-what-was-drawn/
+            https://stackoverflow.com/questions/38316402/how-to-save-a-canvas-as-png-in-selenium
+        '''
+        self.switch_to_console()
+
+        # Get the canvas element
+        canvas = self.selenium.find_element_by_id(canvas_id)
+
+        # Now run some java script to get the contents of the canvas element
+        # base 64 encoded.
+        image_base64_url = self.selenium.execute_script(
+            "return arguments[0].toDataURL('image/png');",
+            canvas
+        )
+
+        # The results will look like:
+        #
+        #   data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAfQAAABkCAYAAABwx8J9AA...
+        #
+        # So parse out the data from the non image data from the URL:
+        image_base64 = image_base64_url.split(",")[1]
+
+        # Now convert to binary:
+        image_png = base64.b64decode(image_base64)
+
+        self.switch_to_appliance()
+        return image_png
+
+    def is_connected(self):
+        '''
+        Waits for the banner on the console to say the console is connected.
+        '''
+        banner = self.get_banner()
+        return re.match('Connected', banner) is not None
+
+    def send_keys(self, text):
+        '''
+        Sends text to the console.
+        '''
+        self.switch_to_console()
+        canvas = self.selenium.find_element_by_id(canvas_id)
+        canvas.send_keys(text)
+        self.switch_to_appliance()
+
+    def send_ctrl_alt_delete(self):
+        '''
+        Presses the ctrl-alt-delete button in the console
+        tab.
+        '''
+        self.switch_to_console()
+        ctrl_alt_del_btn = self.selenium.find_element_by_id(ctrl_alt_del_id)
+        ctrl_alt_del_btn.click()
+        self.switch_to_appliance()
+
+    def switch_to_appliance(self):
+        '''
+        Switches focus to appliance tab/window.
+        '''
+        logger.info("Switching to appliance: window handle = {}".format(self.appliance_handle))
+        self.selenium.switch_to_window(self.appliance_handle)
+
+    def switch_to_console(self):
+        '''
+        Switches focus to console tab/window.
+        '''
+        logger.info("Switching to console: window handle = {}".format(self.console_handle))
+        self.selenium.switch_to_window(self.console_handle)
+
+    def wait_for_connect(self, timeout=5):
+        '''
+        Wait's for as long as the specified/default timeout for the console to
+        be connected.
+        '''
+        wait_for(func=lambda: self.is_connected(),
+                 delay=1,
+                 num_sec=timeout)
+
---
+++ cfme/tests/cloud_infra_common/test_html5_vm_console.py
@@ -0,0 +1,99 @@
+import pytest
+import imghdr
+import re
+
+from cfme.cloud.provider.openstack import OpenStackProvider
+from cfme.configure.configuration import VMwareConsoleSupport
+from cfme.infrastructure.provider.rhevm import RHEVMProvider
+from cfme.infrastructure.provider.virtualcenter import VMwareProvider
+from cfme.common.vm import VM
+from utils import testgen, version
+from utils.appliance.implementations.ui import navigate_to
+from utils.conf import credentials
+from utils.log import logger
+
+pytestmark = pytest.mark.usefixtures('setup_provider')
+
+pytest_generate_tests = testgen.generate(
+    [OpenStackProvider, RHEVMProvider, VMwareProvider],
+    scope='module'
+)
+
+
+@pytest.fixture(scope="function")
+def vm_obj(request, provider, setup_provider, small_template, vm_name):
+    vm_obj = VM.factory(vm_name, provider, template_name=small_template)
+
+    @request.addfinalizer
+    def _delete_vm():
+        try:
+            provider.mgmt.delete_vm(vm_obj.name)
+        except Exception:
+            logger.warning("Failed to delete vm `{}`.".format(vm_obj.name))
+
+    vm_obj.create_on_provider(timeout=2400, find_in_cfme=True, allow_skip="default")
+    return vm_obj
+
+
+def _configureVMwareConsoleForTest(appliance):
+    '''
+    Configure VMware Console to use VNC which is what is required
+    for the HTML5 console.
+    '''
+
+    navigate_to(appliance.server, 'Server')
+
+    settings_pg = VMwareConsoleSupport(
+        appliance=appliance,
+        console_type='VNC',
+    )
+    settings_pg.update()
+
+
+@pytest.mark.uncollectif(lambda: version.current_version() < '5.8', reason='Only valid for >= 5.8')
+def test_html5_vm_console(appliance, provider, vm_obj):
+    '''
+    Tests the HTML5 console support for a particular provider.   The supported providers are:
+
+        VMware
+        Openstack
+        RHV
+
+    For a given provider, and a given VM, the console will be opened, and then:
+
+        - The console's status will be checked.
+        - A command that creates a file will be sent through the console.
+        - Using ssh we will check that the command worked (i.e. that the file
+          was created.
+    '''
+    if provider.one_of(VMwareProvider):
+        _configureVMwareConsoleForTest(appliance)
+
+    vm_obj.open_console(console='VM Console')
+    assert vm_obj.vm_console is not None, 'VMConsole object should be created'
+
+    vm_console = vm_obj.vm_console
+
+    # In this case I want to test that we can get banner text without an error.
+    # This tests that the element is there, not what is in the banner.
+    vm_console.get_banner()
+
+    # If the console is healthy, it should connect.
+    vm_console.wait_for_connect()
+
+    # Get the login screen image, and make sure it is a png file:
+    screen = vm_console.get_screen()
+    assert imghdr.what('', screen) == 'png'
+
+    # Try to login:
+    # XXX: This is hard coded, we need to add some new yaml to track the
+    #      template credentials.
+    vm_console.send_keys("cirros\n")
+    vm_console.send_keys("cubswin:)\n")
+
+    # create file on system
+    vm_console.send_keys("touch /tmp/blather\n")
+
+    # Test pressing ctrl-alt-delete...we should be able to get a new connect after doing this:
+    vm_console.send_ctrl_alt_delete()
+    vm_console.wait_for_connect()
